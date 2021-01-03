c979611110d842b2ebcaaaa0628f4c835fe8d63f
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-01-02 23:27:37 -0300

    Manipulando listas de threads

---
+++ src/main/java/br/com/alura/lista/TarefaAdicionarElemento.java
@@ -0,0 +1,21 @@
+package br.com.alura.lista;
+
+public class TarefaAdicionarElemento implements Runnable {
+
+    private Lista lista;
+    private int numeroDoThread;
+
+    public TarefaAdicionarElemento(Lista lista, int numeroDoThread) {
+        this.lista = lista;
+        this.numeroDoThread = numeroDoThread;
+    }
+
+    @Override
+    public void run() {
+
+        for(int i = 0; i < 10; i++){
+            lista.adicionaElementos("Thread " + numeroDoThread + " - "  + i);
+        }
+
+    }
+}
